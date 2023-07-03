b9b25b8a7cd69449df08298779cc6a96fdd7f2eb
Jay Koshti <jay.koshti@simformsolutions.com>
2023-07-03 17:10:00 +0530

    Add Practical9

---
+++ Practical1/HelloWorldTest/MessageTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Practical1.Controllers;
+using System;
+using System.Web.Mvc;
+
+namespace HelloWorldTest
+{
+    [TestClass]
+    public class MessageTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            HomeController homeController = new HomeController();
+
+            ViewResult result = homeController.Index() as ViewResult;
+
+            Assert.AreEqual("Hello World", result.ViewBag.Message);
+        }
+    }
+}
---
+++ Practical1/HelloWorldTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorldTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloWorldTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d7664a9a-a00d-4fd2-bf9f-d105c0d13fb2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

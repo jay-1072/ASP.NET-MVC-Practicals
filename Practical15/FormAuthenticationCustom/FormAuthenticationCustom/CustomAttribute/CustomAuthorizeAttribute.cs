eaf33ced3e20ee9dd4904b80d451f72495d4fe90
Jay Koshti <jay.koshti@simformsolutions.com>
2023-07-05 18:05:25 +0530

    Add Practical15

---
+++ Practical15/FormAuthenticationCustom/FormAuthenticationCustom/CustomAttribute/CustomAuthorizeAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormAuthenticationCustom.CustomAttribute
+{
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+
+            if (!filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                // User is authenticated but doesn't have access
+                filterContext.Result = new ViewResult
+                {
+                    ViewName = "~/Account/Login" // Access denied view name
+                };
+            }
+
+            if (filterContext.Result is HttpUnauthorizedResult)
+            {
+                filterContext.Result = new RedirectResult("~/Account/Denied");
+            }
+        }
+
+    }
+}
---
+++ Practical15/FormAuthenticationCustom/FormAuthenticationCustom/Models/Membership.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormAuthenticationCustom.Models
+{
+    public class Membership
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
